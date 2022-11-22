9adccd28438f9925034c28229edd1d87d0d35ffd
Katarzyna <kanowory@student.wszib.edu.pl>
2022-11-22 20:32:27 +0100

    Initial commit

---
+++ src/NumberPresenter.java
@@ -0,0 +1,33 @@
+public class NumberPresenter {
+        public static void main(String[] args) {
+
+            int i = 123;
+
+            showInt(i);
+            showIntAsBinary(i);
+            showIntAsOctal(i);
+            showIntAsHex(i);
+        }
+
+        public static void showInt(int i) {
+            System.out.print("Dec: ");
+            System.out.printf("%32s%n", i);
+            System.out.println("--------------------------------------------");
+        }
+
+        public static void showIntAsBinary(int i) {
+            System.out.print("Bin: ");
+            System.out.printf("%32s%n",Integer.toBinaryString(i));
+        }
+
+        public static void showIntAsOctal(int i) {
+            System.out.print("Oct: ");
+            System.out.printf("%32s%n",Integer.toOctalString(i));
+        }
+
+        public static void showIntAsHex(int i) {
+            System.out.print("Hex: ");
+            System.out.printf("%32s%n", Integer.toHexString(i));
+        }
+    }
+
