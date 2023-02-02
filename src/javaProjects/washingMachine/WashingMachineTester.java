a4df33156a70eeb8ce051d889c50a20caf9aebe2
Katarzyna <kanowory@student.wszib.edu.pl>
2023-02-02 13:29:32 +0100

    new project

---
+++ src/javaProjects/washingMachine/WashingMachineTester.java
@@ -0,0 +1,13 @@
+package javaProjects.washingMachine;
+
+public class WashingMachineTester {
+    public static void main(String[] args) {
+        WashingMachine washingMachine = new WashingMachine();
+
+        washingMachine.modeForSensitiveClothes();
+        System.out.println(washingMachine);
+
+        washingMachine.modeForTowelsAndBedLinen();
+        System.out.println(washingMachine);
+    }
+}
